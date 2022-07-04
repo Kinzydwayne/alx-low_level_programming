538b6d68d9760168e60dd4a8132ae4dbc4f1d8ee
kinzydwayne <kinzydwaynecarter@gmail.com>
2022-07-04 12:47:20 -0700

    My first commit Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,45 @@
 #!/bin/bash
+#include <stdlib.h>
+
+#include <time.h>
+
 #include <stdio.h>
-/
- * main - Entry point
- *  * Return: Always 0 (sucess)
- *   */
- int main(void)
+
+/**
+ *
+ *  *main - prints the alphabet in lowercase, followed by a new line
+ *
+ *   *
+ *
+ *    *Return: Always 0
+ *
+ *     **/
+
+int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+	        char ch;
+
+
+
+		        for (ch = 'a'; ch <= 'z'; ch++)
+
+				        {
+
+						        if (ch != 'q' && ch != 'e')
+
+								        putchar(ch);
+
+							        }
+
+
+
+			        putchar(10);
+
+
+
+				        return (0);
+
 }
 
